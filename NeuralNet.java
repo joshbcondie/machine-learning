b08f6e359901a3976f196adb37a2d29efc200615
Josh <joshbcondie@gmail.com>
2015-10-12 16:37:33 -0600

    Keep track of the MSE and accuracy for both the validation set and the training set

--- NeuralNet.java
+++ NeuralNet.java
@@ -10,8 +10,8 @@ public class NeuralNet extends SupervisedLearner {
 
 	final static int classCount = 3;
 	final static double minAccuracy = 0.85;
-	static int hiddenLayerCount = 2;
-	final static int neuronsPerHiddenLayer = 2;
+	static int hiddenLayerCount = 1;
+	final static int neuronsPerHiddenLayer = 8;
 	final static double learningRate = 0.1;
 	double[][][] hiddenWeights;
 	double[][] outputWeights;
@@ -101,7 +101,10 @@ public class NeuralNet extends SupervisedLearner {
 		features.shuffle(random, labels);
 
 		double[] guessedLabels = new double[labels.cols()];
-		double wrongGuesses = Integer.MAX_VALUE;
+		double validationWrongGuesses = Integer.MAX_VALUE;
+		double trainingWrongGuesses = 0;
+		double validationSSE = 0;
+		double trainingSSE = 0;
 		ArrayList<Double> epochAccuracies = new ArrayList<>();
 		int validationSetNumber = (int) (validationSetSize * features.rows());
 		Matrix validationFeatures = new Matrix(features, 0, 0,
@@ -116,7 +119,10 @@ public class NeuralNet extends SupervisedLearner {
 		double bestEpoch = 0;
 
 		do {
-			wrongGuesses = 0;
+			trainingWrongGuesses = 0;
+			validationWrongGuesses = 0;
+			trainingSSE = 0;
+			validationSSE = 0;
 
 			trainingFeatures.shuffle(random, trainingLabels);
 
@@ -201,16 +207,32 @@ public class NeuralNet extends SupervisedLearner {
 				// System.out.println(deltas[a][j]);
 			}
 
-			for (int i = 0; i < validationFeatures.rows(); i++) {
-				predict(validationFeatures.row(i), guessedLabels);
+			for (int i = 0; i < trainingFeatures.rows(); i++) {
+				double[] outputs = getOutputs(trainingFeatures.row(i));
+				predict(trainingFeatures.row(i), guessedLabels);
 
-				for (int j = 0; j < validationLabels.cols(); j++) {
-					if (validationLabels.get(i, j) != guessedLabels[j])
-						wrongGuesses++;
+				for (int j = 0; j < trainingLabels.cols(); j++) {
+					trainingSSE += (trainingLabels.get(i, j) / (classCount - 1) - outputs[j])
+							* (trainingLabels.get(i, j) / (classCount - 1) - outputs[j]);
+					if (trainingLabels.get(i, j) != guessedLabels[j])
+						trainingWrongGuesses++;
 				}
 			}
 
-			double accuracy = (validationFeatures.rows() - wrongGuesses)
+			for (int i = 0; i < validationFeatures.rows(); i++) {
+				double[] outputs = getOutputs(validationFeatures.row(i));
+				predict(validationFeatures.row(i), guessedLabels);
+
+				for (int j = 0; j < validationLabels.cols(); j++) {
+					validationSSE += (validationLabels.get(i, j)
+							/ (classCount - 1) - outputs[j])
+							* (validationLabels.get(i, j) / (classCount - 1) - outputs[j]);
+					if (validationLabels.get(i, j) != guessedLabels[j])
+						validationWrongGuesses++;
+				}
+			}
+
+			double accuracy = (validationFeatures.rows() - validationWrongGuesses)
 					/ validationFeatures.rows();
 
 			if (accuracy > bestAccuracy) {
@@ -229,7 +251,13 @@ public class NeuralNet extends SupervisedLearner {
 
 			epochAccuracies.add(accuracy);
 			// printWeights();
-			System.out.println(accuracy);
+			System.out.println("Training accuracy: "
+					+ (trainingFeatures.rows() - trainingWrongGuesses)
+					/ trainingFeatures.rows());
+			System.out.println("Validation accuracy: " + accuracy);
+			System.out.println(trainingSSE / trainingFeatures.rows());
+			System.out.println(validationSSE
+					/ validationFeatures.rows());
 		} while (epochAccuracies.size() - bestEpoch < 6
 				|| bestAccuracy < minAccuracy);
 
