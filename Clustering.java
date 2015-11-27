1f86e951e77989f6c35bd7cc6bd6659054dcece4
Josh <joshbcondie@gmail.com>
2015-11-26 22:57:23 -0700

    Finish k-means

--- Clustering.java
+++ Clustering.java
@@ -18,64 +18,124 @@ public class Clustering {
 		}
 
 		double[][] centroids = new double[k][];
-		for (int i = 0; i < k; i++) {
-			centroids[i] = new double[data.cols()];
-			for (int j = 0; j < data.cols(); j++)
-				centroids[i][j] = data.get(i, j);
-		}
-
-		System.out.println("Computing Centroids:");
-		for (int i = 0; i < k; i++) {
-			System.out.print("Centroid " + i + " = ");
-			for (int j = 0; j < data.cols(); j++) {
-				if (centroids[i][j] == Matrix.MISSING)
-					System.out.print("?");
-				else if (data.valueCount(j) == 0)
-					System.out.printf("%.3f", centroids[i][j]);
-				else
-					System.out.print(data.attrValue(j, (int) centroids[i][j]));
-				if (j != data.cols() - 1)
-					System.out.print(", ");
-			}
-			System.out.println();
-		}
-
-		System.out.println("Making Assignments");
-		double sse = 0;
-
+		boolean clustersChanged = true;
 		int[] clusters = new int[data.rows()];
-		for (int i = 0; i < data.rows(); i++) {
-			double minSquaredDistance = Double.MAX_VALUE;
-			int closestK = -1;
-			for (int j = 0; j < k; j++) {
-				double squaredDistance = squaredDistance(data.row(i),
-						centroids[j]);
-				if (squaredDistance < minSquaredDistance) {
-					minSquaredDistance = squaredDistance;
-					closestK = j;
-				}
-			}
-			clusters[i] = closestK;
-			sse += minSquaredDistance;
-			if (i % 10 == 0) {
-				if (i > 0)
-					System.out.println();
-				System.out.print("\t");
-			}
-			System.out.print(i + "=" + closestK + " ");
+
+		for (int i = 0; i < k; i++) {
+			clusters[i] = i;
+			centroids[i] = new double[data.cols()];
 		}
-		System.out.printf("\nSSE: %.3f", sse);
+
+		for (int i = k; i < data.rows(); i++)
+			clusters[i] = -1;
+
+		int iteration = 1;
+
+		while (clustersChanged) {
+
+			clustersChanged = false;
+
+			System.out.println("***************");
+			System.out.println("Iteration " + iteration);
+			System.out.println("***************");
+			System.out.println("Computing Centroids:");
+			for (int i = 0; i < k; i++) {
+				int[] numPoints = new int[data.cols()];
+				double[] sum = new double[data.cols()];
+				int[][] histogram = new int[data.cols()][];
+				for (int c = 0; c < histogram.length; c++)
+					histogram[c] = new int[data.valueCount(c)];
+
+				for (int r = 0; r < data.rows(); r++) {
+					if (clusters[r] == i) {
+						for (int c = 0; c < data.cols(); c++) {
+							if (data.get(r, c) != Matrix.MISSING) {
+								numPoints[c]++;
+								if (data.valueCount(c) == 0)
+									sum[c] += data.get(r, c);
+								else
+									histogram[c][(int) data.get(r, c)]++;
+							}
+						}
+					}
+				}
+				for (int c = 0; c < data.cols(); c++) {
+					double lastValue = centroids[i][c];
+					if (data.valueCount(c) == 0) {
+						if (numPoints[c] > 0)
+							centroids[i][c] = sum[c] / numPoints[c];
+						else
+							centroids[i][c] = Matrix.MISSING;
+					} else {
+						int max = 0;
+						int maxIndex = -1;
+						for (int j = 0; j < data.valueCount(c); j++) {
+							if (histogram[c][j] > max) {
+								max = histogram[c][j];
+								maxIndex = j;
+							}
+						}
+						if (maxIndex >= 0)
+							centroids[i][c] = maxIndex;
+						else
+							centroids[i][c] = Matrix.MISSING;
+					}
+					if (centroids[i][c] != lastValue)
+						clustersChanged = true;
+				}
+				System.out.print("Centroid " + i + " = ");
+				for (int c = 0; c < data.cols(); c++) {
+					if (centroids[i][c] == Matrix.MISSING)
+						System.out.print("?");
+					else if (data.valueCount(c) == 0)
+						System.out.printf("%.3f", centroids[i][c]);
+					else
+						System.out.print(data.attrValue(c,
+								(int) centroids[i][c]));
+					if (c != data.cols() - 1)
+						System.out.print(", ");
+				}
+				System.out.println();
+			}
+
+			System.out.println("Making Assignments");
+			double sse = 0;
+
+			for (int r = 0; r < data.rows(); r++) {
+				double minSquaredDistance = Double.MAX_VALUE;
+				int closestK = -1;
+				for (int i = 0; i < k; i++) {
+					double squaredDistance = squaredDistance(data.row(r),
+							centroids[i]);
+					if (squaredDistance < minSquaredDistance) {
+						minSquaredDistance = squaredDistance;
+						closestK = i;
+					}
+				}
+				clusters[r] = closestK;
+				sse += minSquaredDistance;
+				if (r % 10 == 0) {
+					if (r > 0)
+						System.out.println();
+					System.out.print("\t");
+				}
+				System.out.print(r + "=" + closestK + " ");
+			}
+			System.out.printf("\nSSE: %.3f\n\n", sse);
+
+			iteration++;
+		}
 	}
 
 	private static double squaredDistance(double[] a, double[] b) {
 		double squaredDistance = 0;
-		for (int i = 0; i < a.length; i++) {
-			if (a[i] == Matrix.MISSING || b[i] == Matrix.MISSING)
+		for (int c = 0; c < a.length; c++) {
+			if (a[c] == Matrix.MISSING || b[c] == Matrix.MISSING)
 				squaredDistance++;
 			else {
-				if (data.valueCount(i) == 0)
-					squaredDistance += (a[i] - b[i]) * (a[i] - b[i]);
-				else if (a[i] != b[i])
+				if (data.valueCount(c) == 0)
+					squaredDistance += (a[c] - b[c]) * (a[c] - b[c]);
+				else if (a[c] != b[c])
 					squaredDistance++;
 			}
 		}
